dba1f2f1f28cb5a4115f30b9b6c8a31f693023d7
gumer <deadjuk@gmail.com>
2022-10-16 22:17:14 +0300

    Initial Commit

---
+++ VK Client Oct/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK Client Oct
+//
+//  Created by Залялиева Эльмира on 16.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
